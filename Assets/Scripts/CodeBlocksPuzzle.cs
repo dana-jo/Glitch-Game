7ee937e9abfc53647470263b4c265bcf26ea1114
Daunchi <dana.joukhadar3@gmail.com>
2026-07-30 23:50:27 +0300

    making Code-Blocks Puzzle's Pallet Area and Sequence Area

---
+++ Assets/Scripts/CodeBlocksPuzzle.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CodeBlocksPuzzle : ObjectiveBehaviour
+{
+    
+    void Start()
+    {
+        OnStart();
+
+    //    /* our code */
+    }
+
+    
+    public override void UpdatePuzzleState()
+    {
+    //    /* our code */
+    }
+
+    public override void RestoreCompletedState()
+    {
+        base.RestoreCompletedState();
+
+    //    /* our code */
+    }
+}
