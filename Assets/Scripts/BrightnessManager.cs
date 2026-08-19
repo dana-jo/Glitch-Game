e963dff6d467a31e469652dcc6b374a6d1dccb64
sarahzarzour <sarah.zarzour5@gmail.com>
2026-08-19 04:01:14 +0300

    added settings and pause menu UI

--- Assets/Scripts/BrightnessManager.cs
+++ Assets/Scripts/BrightnessManager.cs
@@ -6,6 +6,8 @@ public class BrightnessManager : MonoBehaviour
     [SerializeField] private Slider brightnessSlider;
     [SerializeField] private Image brightnessPanel;
     [SerializeField] private float maxAlpha = 0.7f;
+    [SerializeField] private Image brightnessBarImage;
+    [SerializeField] private Sprite[] brightnessBarSprites;
 
     void Start()
     {
@@ -17,5 +19,18 @@ public class BrightnessManager : MonoBehaviour
         float alpha = 1 - brightnessSlider.value;
         alpha = Mathf.Clamp(alpha, 0f, maxAlpha);
         brightnessPanel.color = new Color(0, 0, 0, alpha);
+
+        int index = Mathf.RoundToInt(brightnessSlider.value * 10);
+        brightnessBarImage.sprite = brightnessBarSprites[index];
+    }
+
+    public void IncreaseBrightness()
+    {
+        brightnessSlider.value = Mathf.Clamp01(brightnessSlider.value + 0.1f);
+    }
+
+    public void DecreaseBrightness()
+    {
+        brightnessSlider.value = Mathf.Clamp01(brightnessSlider.value - 0.1f);
     }
 }
--- Assets/Scripts/SoundEffectManager.cs
+++ Assets/Scripts/SoundEffectManager.cs
@@ -13,6 +13,21 @@ public class SoundEffectManager : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Toggle musicMuteToggle;
     [SerializeField] private AudioClip defaultMusicTrack;
+
+    //for UI
+    //music
+    [SerializeField] private Image muteButtonImage;
+    [SerializeField] private Sprite muteSprite;
+    [SerializeField] private Sprite unmuteSprite;
+    [SerializeField] private Image volumeBarImage;
+    [SerializeField] private Sprite[] volumeBarSprites;
+    //sfx
+    [SerializeField] private Image sfxVolumeBarImage;
+    [SerializeField] private Image sfxMuteButtonImage;
+
+    private float lastSfxVolume = 1f;
+    private bool isSfxMuted = false;
+
     private float lastMusicVolume = 1f;
     private bool isMusicMuted = false;
     private void Awake()
@@ -66,7 +81,16 @@ public class SoundEffectManager : MonoBehaviour
 
     public void OnVolumeChanged()
     {
+        if (isSfxMuted && sfxSlider.value > 0f)
+        {
+            isSfxMuted = false;
+        }
+
+        lastSfxVolume = sfxSlider.value;
+
         SetVolume(sfxSlider.value);
+
+        UpdateSfxUI();
     }
 
     public static void PlayMusic(AudioClip clip)
@@ -97,6 +121,9 @@ public class SoundEffectManager : MonoBehaviour
         }
         lastMusicVolume = musicSlider.value;
         SetMusicVolume(musicSlider.value);
+
+        int index = Mathf.RoundToInt(musicSlider.value * 10);
+        volumeBarImage.sprite = volumeBarSprites[index];
     }
     public void OnMusicMuteToggled(bool isMuted)
     {
@@ -112,5 +139,68 @@ public class SoundEffectManager : MonoBehaviour
             SetMusicVolume(lastMusicVolume > 0 ? lastMusicVolume : 1f);
             musicSlider.SetValueWithoutNotify(lastMusicVolume > 0 ? lastMusicVolume : 1f);
         }
+        int index = Mathf.RoundToInt(musicSlider.value * 10);
+        volumeBarImage.sprite = volumeBarSprites[index];
+
+        muteButtonImage.sprite = isMusicMuted ? unmuteSprite : muteSprite;
+    }
+    public void IncreaseMusicVolume()
+    {
+        musicSlider.value = Mathf.Clamp01(musicSlider.value + 0.1f);
+    }
+
+    public void DecreaseMusicVolume()
+    {
+        musicSlider.value = Mathf.Clamp01(musicSlider.value - 0.1f);
+    }
+
+    public void ToggleMusicMute()
+    {
+        OnMusicMuteToggled(!isMusicMuted);
+    }
+
+    public void IncreaseSfxVolume()
+    {
+        sfxSlider.value = Mathf.Clamp01(sfxSlider.value + 0.1f);
+    }
+
+    public void DecreaseSfxVolume()
+    {
+        sfxSlider.value = Mathf.Clamp01(sfxSlider.value - 0.1f);
+    }
+
+    public void ToggleSfxMute()
+    {
+        isSfxMuted = !isSfxMuted;
+
+        if (isSfxMuted)
+        {
+            int index = Mathf.RoundToInt(sfxSlider.value * 10);
+            sfxVolumeBarImage.sprite = volumeBarSprites[index];
+
+            lastSfxVolume = sfxSlider.value > 0 ? sfxSlider.value : lastSfxVolume;
+
+            SetVolume(0f);
+            sfxSlider.SetValueWithoutNotify(0f);
+        }
+        else
+        {
+            float volume = lastSfxVolume > 0 ? lastSfxVolume : 1f;
+
+            SetVolume(volume);
+            sfxSlider.SetValueWithoutNotify(volume);
+        }
+
+        UpdateSfxUI();
+    }
+
+    private void UpdateSfxUI()
+    {
+        int index = Mathf.RoundToInt(sfxSlider.value * 10);
+
+        sfxVolumeBarImage.sprite = volumeBarSprites[index];
+
+        sfxMuteButtonImage.sprite =
+            isSfxMuted ? unmuteSprite : muteSprite;
     }
 }
